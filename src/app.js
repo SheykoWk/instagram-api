ced9ebb25b5ff28b86c22bbfbc1d5dcc68f061e2
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2023-05-04 19:59:23 -0600

    feat(documentation) generate swagger docs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require('cors')
 const path = require('path')
+const swaggerUi = require('swagger-ui-express')
 require('dotenv').config()
 
 const userRouter = require('./users/users.router')
@@ -11,6 +12,8 @@ const followRouter = require('./follows/follows.router')
 const upload = require('./middlewares/multer.middleware')
 const db = require('./utils/database')
 const initModels = require('./models/initModels')
+const swaggerDoc = require('../swagger.json')
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -50,12 +53,13 @@ app.get('/api/v1/uploads/:fileName', (req, res) => {
     res.status(200).sendFile(path.resolve('public/') + '/' + req.params.fileName)
 })
 
+
 app.post('/api/v1/add-file', upload.single('my-image') ,(req, res) => {
 
     res.status(200).json({message: req.file})
 })
 
-
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/posts', postRouter)
